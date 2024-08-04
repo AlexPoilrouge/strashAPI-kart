d8b058a7d742f646275b6063b62131e3333305f5
Alexandre Hurstel <alexandre.hurstel@mailoo.org>
2024-08-04 16:11:32 +0200

    Rework ongoign: Addons diasable/rm -> need to be pending op

--- src/addons/util.js
+++ src/addons/util.js
@@ -2,12 +2,17 @@
 const fs= require('fs')
 const path = require('path');
 
+const { FileMutex } = require('../kart_util')
 
-const addons_config= require("../../config/addons.json")
+
+const addons_config= require("../../config/addons.json");
+const { unlock } = require('proper-lockfile');
 
 const MB_size= 1024 * 1024;
 
 let hereLog= (...args) => {console.log("[utils]", ...args);};
+
+const pendingOpFilename= "pending_op.json"
 
 
 function getKarters(){
@@ -37,6 +42,25 @@ function _ensureDirectory(in_path){
     }
 }
 
+function _ensureFile(filePath){
+    try {
+        if (fs.existsSync(filePath)) {
+            if (fs.statSync(filePath).isFile()) {
+                return true;
+            } else {
+                hereLog(`[ensureFile](${path}) - path exists as a directory…`)
+                return false;
+            }
+        } else {
+            fs.writeFileSync(filePath, '');
+            return true;
+        }
+    } catch (error) {
+        hereLog(`[ensureFile](${path}) - ${error}`)
+        return false;
+    }
+}
+
 function _getKarterSubdir(karter, dirbasename, ensure){
     const dirpath= path.join(addons_config.racers[karter].directory, dirbasename)
 
@@ -45,8 +69,22 @@ function _getKarterSubdir(karter, dirbasename, ensure){
     return dirpath
 }
 
+function _getKarterSubfile(karter, subpath, ensure){
+    const filepath= path.join(addons_config.racers[karter].directory, subpath)
+
+    if(ensure){ 
+        _ensureDirectory(path.dirname(dirpath))
+
+        if(!_ensureFile(filepath)) return undefined
+    }
+
+    return filepath
+}
+
 let getInstalledDir = (karter, ensure=false) => _getKarterSubdir(karter, 'installed', ensure)
 let getEnabledDir = (karter, ensure=false) => _getKarterSubdir(karter, 'enabled', ensure)
+
+let getAddonPendingOpFile = (karter, ensure=false) => _getKarterSubfile(karter, pendingOpFilename, ensure)
 
 
 function _listAddonDir(addonDirectory, filter=null){
@@ -79,6 +117,104 @@ const _isAddonListed= (addonFile_list, addonName) => addonFile_list && Boolean(
 const isAddonInstalled= (karter, addonName) => _isAddonListed(listInstalledAddons(karter), addonName)
 const isAddonEnabled= (karter, addonName) => _isAddonListed(listEnabledAddons(karter), addonName)
 
+function _fetchPendingOpData(karter){
+    let empty= { deletion: [], disablement: [] }
+
+    try {
+        let pendingOp_filepath= getAddonPendingOpFile(karter, true)
+        if(!pendingOp_filepath) return empty;
+
+        const absolutePath = path.resolve(filePath); // Ensure the path is absolute
+        const data = fs.readFileSync(absolutePath, 'utf8'); // Read the file as a string
+
+        if(!data.deletion) data.deletion= []
+        if(!data.disablement) data.disablement= []
+
+        return JSON.parse(data); // Parse the JSON string to an object
+    } catch (error) {
+        hereLog(`[fetchPendingOpData]{${karter}} error - ${error}`);
+        
+        return empty
+    }
+}
+
+function _setPendingOpData(karter, pendingOp_obj){
+    let pendingOp_filepath= getAddonPendingOpFile(karter, true)
+    if(!pendingOp_filepath) throw Error(`Cant' save pending op data to corresponding file (for ${karter})`)
+
+    try {
+        const jsonData = JSON.stringify(jsonObject, null, 2); // Convert object to JSON string with pretty print
+        fs.writeFileSync(pendingOpFilename, jsonData, 'utf8'); // Write JSON data to the file
+        console.log(`Data successfully written to ${absolutePath}`);
+    } catch (error) {
+        hereLog(`[fetchPendingOpData]{${karter}} can't write pending op data - ${error}`);
+        throw error;
+    }
+}
+
+function _removePendingOpData(in_out_pendingOp_data, op, addon_filename){
+    var notfound_check= true
+    in_out_pendingOp_data[op]= in_out_pendingOp_data[op].filter(fn => {
+        let b= (fn != addon_filename)
+        notfound_check= notfound_check && b
+        return b
+    })
+
+    // if(!notfound_check) _setPendingOpData(karter, data)
+    return !notfound_check
+}
+
+function _addPendingOpData(in_out_pendingOp_data, op, addon_filename){
+    var found_check= Boolean(in_out_pendingOp_data[op].find(addon_filename))
+
+    if(!found_check){
+        in_out_pendingOp_data[op].push(addon_filename)
+        return true
+    }
+    return false
+}
+
+async function addPendingOp(karter, op, addon_filename){
+    let lock= new FileMutex(getAddonPendingOpFile(karter))
+    await lock.LockWait()
+
+    var data= _fetchPendingOpData(karter)
+    let other_op= (op==="disablement")? "deletion" : "disablement"
+
+    var change= false
+    change= change || _rmPendingOpData(data, other_op, addon_filename)
+    change= change || _addPendingOpData(data, op, addon_filename)
+
+    if(change) _setPendingOpData(karter, data)
+
+    lock(unlock)
+
+    return data
+}
+
+async function rmPendingOp(karter, op, addon_filename){
+    let lock= new FileMutex(getAddonPendingOpFile(karter))
+    await lock.LockWait()
+
+    var data= _fetchPendingOpData(karter)
+
+    if(_removePendingOpData(data, op, addon_filename))
+        _setPendingOpData(karter, data)
+
+    lock.Unlock()
+
+    return data
+}
+
+function _hasPendingOp(karter, addon_filename, op){
+    var data= _fetchPendingOpData(karter)
+
+    return Boolean(data[op].find(fn => fn === addon_filename))
+}
+let isAddonDeletionPending = (karter, addon_filename) => _hasPendingOp(karter, addon_filename, "deletion")
+let isAddonDisablementPending = (karter, addon_filename) => _hasPendingOp(karter, addon_filename, "disablement")
+
 module.exports= { MB_size, getKarters, karterReqCheck, getInstalledDir, getEnabledDir,
+                    getAddonPendingOpFile, addPendingOp, rmPendingOp, isAddonDeletionPending, isAddonDisablementPending,
                     listInstalledAddons, listEnabledAddons, isAddonInstalled, isAddonEnabled
                 }
--- src/kart_util.js
+++ src/kart_util.js
@@ -1,5 +1,7 @@
 
 const child_process= require("child_process");
+const lockfile = require('proper-lockfile');
+const path = require('path')
 
 
 function execute_sh_command(sh_cmd_string, timeout=0){
@@ -31,5 +33,51 @@ let _errHandle= (err, fallbackFn) =>    {  if((Boolean(err)) && Boolean(err.stat
     else return fallbackFn(err);
 }
 
+class FileMutex{
+    constructor(filepath){
+        this.filepath= path.resolve(filepath)
+    }
 
-module.exports= {execute_sh_command, _errHandle}
+    Lock(){
+        if(this.filepath)
+            lockfile.lockSync(this.filepath)
+    }
+
+    _lockWait(time_out){
+        return new Promise(resolve => {
+            setTimeout(resolve, time_out)
+        })
+    }
+
+    async LockWait(time_out_sec=60){
+        const MAX_TIME= time_out_sec*1000
+        var ellapsed=0
+        let step= 33
+
+        let locked= false
+        while(ellapsed<MAX_TIME){
+            if(this.isLocked()){
+                await this._lockWait(step)
+                ellapsed+= step
+            }
+            else{
+                this.Lock()
+                locked= true
+                break
+            }
+        }
+
+        if(!locked) throw new Error(`Couldn't get file lock on file '${this.filepath}'…`)
+    }
+
+    Unlock(){
+        if(this.filepath)
+            lockfile.unlockSync(this.filepath)
+    }
+
+    isLocked(){
+        return this.filepath() && lockfile.checkSync(this.filepath)
+    }
+}
+
+module.exports= {execute_sh_command, _errHandle, FileMutex}
